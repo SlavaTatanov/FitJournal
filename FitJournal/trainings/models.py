aa0d2e7838cbcce25a027ea9a3dbaa1334c27f87
SlavaTatanov <slavatatanov@gmail.com>
2023-06-13 18:50:37 +0500

    base models

--- FitJournal/trainings/models.py
+++ FitJournal/trainings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class TypeExercise(models.Model):
+    """
+    Класс описывающий типы упражнений
+    """
+    name = models.CharField(max_length=128)  # Название типа упражнений
+    description = models.TextField()  # Описание типа упражнений
+
+
+class Exercise(models.Model):
+    """
+    Модель описывающая упражнение
+    """
+    name = models.CharField(max_length=128)  # Название упражнения
+    description = models.TextField()  # Описание упражнения
+    type = models.ForeignKey(to=TypeExercise, on_delete=models.PROTECT, default=0)
