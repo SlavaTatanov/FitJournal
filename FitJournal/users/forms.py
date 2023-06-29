7cbe4a0619a235bda3b13461b9d4d7827f07c9fe
SlavaTatanov <slavatatanov@gmail.com>
2023-06-29 20:48:55 +0500

    auth form

--- FitJournal/users/forms.py
+++ FitJournal/users/forms.py
@@ -1,8 +1,19 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': "form-control mt-2",
+        'placeholder': "Введите имя пользователя"
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': "form-control mt-2",
+        'placeholder': "Введите пароль"
+    }))
+
     class Meta:
         model = User
         fields = ('username', 'password')
--- FitJournal/users/views.py
+++ FitJournal/users/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-from users.forms import AuthenticationForm
+from users.forms import UserLoginForm
 
 
 def login(request: HttpRequest):
-    context = {'form': AuthenticationForm()}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)  # Создаем форму и передаем в нее дату, для дальнейшей обработки
+        if form.is_valid():  # Проверяем валидность
+            username = request.POST['username']
+            password = request.POST['password']  # Берем значения из формы
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                print("Авторизуем")
+                auth.login(request, user)  # Если такой юзер есть, то авторизуем
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                print("Такого юзера нет")
+    context = {'form': UserLoginForm()}
     return render(request, 'users/login.html', context)
+
+
