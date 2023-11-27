67c401f9f4adfc40cad3bbf71bdb653172453a71
SlavaTatanov <slavatatanov@gmail.com>
2023-11-27 18:19:40 +0500

    Рефакторинг шаблонов

--- FitJournal/users/models.py
+++ FitJournal/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.utils import timezone
 
 
 class UserProfile(models.Model):
@@ -14,7 +15,7 @@ class UserProfile(models.Model):
         male = ("М", "Муж.")
         female = ("Ж", "Жен.")
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     height = models.IntegerField(blank=True, null=True)
     gender = models.CharField(max_length=1,
                               choices=Gender.choices,
@@ -28,3 +29,19 @@ class UserProfile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.userprofile.save()
+
+
+class UserWeight(models.Model):
+    """
+    Модель описывающая вес пользователя в определенный промежуток времени
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=False)
+    weight_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        """
+        Индексы для модели
+        """
+        indexes = [
+            models.Index(fields=['user', '-weight_date'])
+        ]
