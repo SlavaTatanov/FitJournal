9de30de2b7c47a79d4e4648ffdaf2e5a333ac921
SlavaTatanov <slavatatanov@gmail.com>
2024-01-07 12:23:41 +0500

    Реализация смены пароля

--- FitJournal/users/urls.py
+++ FitJournal/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -6,6 +6,10 @@ app_name = 'users'
 
 urlpatterns = [
     path('password_change/',
-         auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'),
-         name='password_change')
+         auth_views.PasswordChangeView.as_view(template_name='users/password_change.html',
+                                               success_url=reverse_lazy('users:password_change_done')),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+         name='password_change_done')
 ]
