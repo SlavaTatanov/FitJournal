6bf3c5334d3276a7336de1753e8a175a5bac2c16
SlavaTatanov <slavatatanov@gmail.com>
2024-03-19 20:58:40 +0500

    Представление для запроса тренировок

--- FitJournal/trainings/views.py
+++ FitJournal/trainings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
+from users.models import UserTraining
 
 
 def index(request):
@@ -15,4 +16,10 @@ def trainings_list(req: HttpRequest):
     """
     Представление дает информацию о тренировках пользователя
     """
-    return render(req, 'trainings/trainings_list.html')
+    # Определяем пользователя и делаем запрос его тренировок
+    user = req.user
+    training_data = UserTraining.objects.filter(user=user).order_by('-training_date')
+
+    # Собираем контекст
+    context = {"training_data": training_data}
+    return render(req, 'trainings/trainings_list.html', context=context)
